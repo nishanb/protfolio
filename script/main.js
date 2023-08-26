d51f32c0387cd0efc10600fc6968978d5bef04c6
Nishan <nishanbeleri@gmail.com>
2023-08-26 20:11:10 +0530

    Alter style

--- script/main.js
+++ script/main.js
@@ -5,7 +5,7 @@ const lastExecutionTimestamp = sessionStorage.getItem('lastExecutionTimestamp');
 const currentDate = new Date();
 
 // If there's no stored timestamp or the stored timestamp is from a previous day, execute the fetch and update the stored timestamp in session storage
-if (!lastExecutionTimestamp || currentDate.getDate() - new Date(lastExecutionTimestamp).getDate() > 0) {
+if (!lastExecutionTimestamp || (currentDate.getDate() - new Date(lastExecutionTimestamp).getDate()) > 0) {
     fetch('https://eool6f1uvkdvuh0.m.pipedream.net/?project=website');
     sessionStorage.setItem('lastExecutionTimestamp', currentDate);
 }
